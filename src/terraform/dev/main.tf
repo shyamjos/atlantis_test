e7d0a21c207dd1faf1495d059d63743e6f048faf
Shyam Jos  <shyam.pp@fullcontact.com>
2023-04-26 17:12:04 +0530

    add rs

--- src/terraform/dev/main.tf
+++ src/terraform/dev/main.tf
@@ -19,4 +19,8 @@ resource "null_resource" "cluster-dev" {
 
 resource "null_resource" "cluster-dev2" {
   # Changes to any instance of the cluster requires re-provisioning
+  }
+
+resource "null_resource" "cluster-dev3" {
+  # Changes to any instance of the cluster requires re-provisioning
   }    
