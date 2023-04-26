bb48c1a301f156af76dbea74cb2140810fca8da8
Shyam Jos  <shyam.pp@fullcontact.com>
2023-04-26 16:00:19 +0530

    move files

---
+++ src/terraform/prod/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "null_resource" "cluster-prod" {
+  # Changes to any instance of the cluster requires re-provisioning
+  }
